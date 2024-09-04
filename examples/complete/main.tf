e96f970a80e33947b19f813ffc9dd42239ecf03b
Jimish Fotariya <fotariyajimish@gmail.com>
2024-09-04 18:35:09 +0530

    fix: context

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,3 +1,10 @@
+module "example_context" {
+  source     = "registry.terraform.io/SevenPico/context/null"
+  version    = "2.0.0"
+  context    = module.context.self
+  enabled    = module.context.enabled
+  attributes = []
+}
 
 module "sns" {
   source  = "SevenPico/sns/aws"
@@ -7,9 +14,8 @@ module "sns" {
 
 module "cloudwatch_event" {
   source = "../../"
-
+  context =  module.example_context.self
   cloudwatch_event_rule_description = var.cloudwatch_event_rule_description
   cloudwatch_event_rule_pattern     = var.cloudwatch_event_rule_pattern
   cloudwatch_event_target_arn       = module.sns.topic_arn
-  context                           = module.context.self
 }
--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "rule_context" {
+module "eventbridge_context" {
   source  = "SevenPico/context/null"
   version = "2.0.0"
 
@@ -8,9 +8,9 @@ module "rule_context" {
 }
 
 resource "aws_cloudwatch_event_rule" "event_rule" {
-  count       = module.rule_context.enabled ? 1 : 0
-  name        = module.rule_context.id
-  description = var.cloudwatch_event_rule_description != "" ? var.cloudwatch_event_rule_description : module.rule_context.id_full
+  count       = module.eventbridge_context.enabled ? 1 : 0
+  name        = module.eventbridge_context.id
+  description = var.cloudwatch_event_rule_description != "" ? var.cloudwatch_event_rule_description : module.eventbridge_context.id_full
   event_bus_name = var.source_event_bus_name
   event_pattern = jsonencode(var.cloudwatch_event_rule_pattern)
 
@@ -18,7 +18,7 @@ resource "aws_cloudwatch_event_rule" "event_rule" {
 }
 
 resource "aws_cloudwatch_event_target" "event_target" {
-  count       = module.rule_context.enabled ? 1 : 0
+  count       = module.eventbridge_context.enabled ? 1 : 0
 
   rule      = try(aws_cloudwatch_event_rule.event_rule[0].name, "")
   target_id = var.cloudwatch_event_target_id

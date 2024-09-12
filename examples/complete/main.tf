e2013d7a3d1808dce2b078ee43e4fb5156b33bb2
Jimish Fotariya <fotariyajimish@gmail.com>
2024-09-12 16:27:44 +0530

    fix: main

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -6,11 +6,12 @@ module "example_context" {
   enabled    = module.context.enabled
   attributes = []
 }
-locals {
-  sns_topic_allowed_aws_services_for_sns_published = ["events.amazonaws.com"]
 
-  cloudwatch_event_rule_description = "This is event rule description."
-  cloudwatch_event_rule_pattern = {
+module "cloudwatch_event" {
+  source = "../../"
+  context =  module.example_context.self
+
+  cloudwatch_event_rule_pattern     = {
     source = ["aws.health"],
     detail-type = ["AWS Health Event"],
     detail = {
@@ -18,14 +19,6 @@ locals {
       eventTypeCategory = ["issue"]
     }
   }
-}
-
-
-module "cloudwatch_event" {
-  source = "../../"
-  context =  module.example_context.self
-
-  cloudwatch_event_rule_pattern     = local.cloudwatch_event_rule_pattern
-  cloudwatch_event_rule_description = local.cloudwatch_event_rule_description
+  cloudwatch_event_rule_description = "This is event rule description."
   cloudwatch_event_target_arn       = module.sns.topic_arn
 }

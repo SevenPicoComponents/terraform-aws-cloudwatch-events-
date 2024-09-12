2c69160f599b69331399c91b1795882ab1820932
Jimish Fotariya <fotariyajimish@gmail.com>
2024-09-12 16:28:12 +0530

    fix: fmt

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -8,14 +8,14 @@ module "example_context" {
 }
 
 module "cloudwatch_event" {
-  source = "../../"
-  context =  module.example_context.self
+  source  = "../../"
+  context = module.example_context.self
 
-  cloudwatch_event_rule_pattern     = {
-    source = ["aws.health"],
+  cloudwatch_event_rule_pattern = {
+    source      = ["aws.health"],
     detail-type = ["AWS Health Event"],
     detail = {
-      service = ["EC2"],
+      service           = ["EC2"],
       eventTypeCategory = ["issue"]
     }
   }
--- main.tf
+++ main.tf
@@ -3,28 +3,28 @@ module "eventbridge_context" {
   version = "2.0.0"
 
   id_length_limit = 64
-  context = module.context.self
-  enabled = module.context.enabled
-  attributes = []
+  context         = module.context.self
+  enabled         = module.context.enabled
+  attributes      = []
 }
 
 resource "aws_cloudwatch_event_rule" "event_rule" {
-  count       = module.eventbridge_context.enabled ? 1 : 0
-  name        = module.eventbridge_context.id
-  description = var.cloudwatch_event_rule_description != "" ? var.cloudwatch_event_rule_description : module.eventbridge_context.id_full
+  count          = module.eventbridge_context.enabled ? 1 : 0
+  name           = module.eventbridge_context.id
+  description    = var.cloudwatch_event_rule_description != "" ? var.cloudwatch_event_rule_description : module.eventbridge_context.id_full
   event_bus_name = var.source_event_bus_name
-  event_pattern = jsonencode(var.cloudwatch_event_rule_pattern)
+  event_pattern  = jsonencode(var.cloudwatch_event_rule_pattern)
 
   tags = module.context.tags
 }
 
 resource "aws_cloudwatch_event_target" "event_target" {
-  count       = module.eventbridge_context.enabled ? 1 : 0
+  count = module.eventbridge_context.enabled ? 1 : 0
 
-  rule      = try(aws_cloudwatch_event_rule.event_rule[0].name, "")
-  target_id = var.cloudwatch_event_target_id
-  arn       = var.cloudwatch_event_target_arn
-  role_arn  = var.cloudwatch_event_target_role_arn
+  rule           = try(aws_cloudwatch_event_rule.event_rule[0].name, "")
+  target_id      = var.cloudwatch_event_target_id
+  arn            = var.cloudwatch_event_target_arn
+  role_arn       = var.cloudwatch_event_target_role_arn
   event_bus_name = var.target_event_bus_name
 }
 
